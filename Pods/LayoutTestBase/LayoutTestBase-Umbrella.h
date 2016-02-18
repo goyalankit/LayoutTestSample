f53a5d8bab7e4d6eb1a545caa0a6b7b3b5c2f90c
Ankit Goyal <angoyal@linkedin.com>
2016-02-18 10:53:50 -0800

    Adding custom module map

---
+++ Pods/LayoutTestBase/LayoutTestBase-Umbrella.h
@@ -0,0 +1,14 @@
+//
+//  LayoutTest-Umbrella.h
+//  Pods
+//
+//  Created by Ankit Goyal on 2/18/16.
+//
+//
+
+#ifndef LayoutTest_Umbrella_h
+#define LayoutTest_Umbrella_h
+
+
+#import "LYTLayoutPropertyTester.h"
+#endif /* LayoutTest_Umbrella_h */
